157172b14c6236d93528d332fe16d37b1f15c4b9
Andres-XL <amovilla1103@gmail.com>
2020-05-14 13:03:04 -0500

    built

---
+++ build/precache-manifest.8bd075486e7db728d681ee08b365d786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f371e8b63914fa8d828719fb54e020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5a602def42a105c974",
+    "url": "/static/css/main.04d575b4.chunk.css"
+  },
+  {
+    "revision": "cb16ba29855952a8bd6b",
+    "url": "/static/js/2.72befd82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72befd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5a602def42a105c974",
+    "url": "/static/js/main.cac87ecf.chunk.js"
+  },
+  {
+    "revision": "30208943052f2b9a183d",
+    "url": "/static/js/runtime-main.783a17a8.js"
+  }
+]);
